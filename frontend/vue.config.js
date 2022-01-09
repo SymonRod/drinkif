c0c710a485b5732e4de813125665ceda2def20b7
SymonRod <simone.rodino@gmail.com>
2022-01-09 20:17:07 +0100

    Modified service worker

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,46 +2,48 @@
 const path = require('path');
 
 module.exports = {
-  // Should be STATIC_URL + path/to/build
-  publicPath: '/static/src/vue/dist/',
+    // Should be STATIC_URL + path/to/build
+    publicPath: '/static/src/vue/dist/',
 
-  // Output to a directory in STATICFILES_DIRS
-  outputDir: path.resolve(__dirname, '../backend/drinkif/static/src/vue/dist/'),
+    // Output to a directory in STATICFILES_DIRS
+    outputDir: path.resolve(__dirname, '../backend/drinkif/static/src/vue/dist/'),
 
-  // Django will hash file names, not webpack
-  filenameHashing: true,
+    // Django will hash file names, not webpack
+    filenameHashing: true,
 
-  // See: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
-  runtimeCompiler: true,
+    // See: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
+    runtimeCompiler: true,
 
-  devServer: {
-    writeToDisk: true, // Write files to disk in dev mode, so Django can serve the assets
-  },
+    devServer: {
+        writeToDisk: true, // Write files to disk in dev mode, so Django can serve the assets
+    },
 
-  pwa: {
-    name: 'Drinkif',
-    themeColor: '#00bad3',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    display: 'fullscreen',
+    pwa: {
+        name: 'Drinkif',
+        themeColor: '#00bad3',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        display: 'fullscreen',
 
+        // configure the workbox plugin
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: 'src/registerServiceWorker.js',
+            // ...other Workbox options...
+        }
+    },
 
-
-    workboxOptions: {
-      skipWaiting: true,
+    pluginOptions: {
+      i18n: {
+        locale: 'en',
+        fallbackLocale: 'en',
+        localeDir: 'locales',
+        enableLegacy: false,
+        runtimeOnly: false,
+        compositionOnly: false,
+        fullInstall: true
+      }
     }
-  },
-
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableLegacy: false,
-      runtimeOnly: false,
-      compositionOnly: false,
-      fullInstall: true
-    }
-  }
 };
