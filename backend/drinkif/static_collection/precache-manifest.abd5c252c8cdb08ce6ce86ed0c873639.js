64507e29d90e67476b6a8b780344988799d69e69
SymonRod <simone.rodino@gmail.com>
2022-01-18 15:40:48 +0100

    PWA Stuff

---
+++ backend/drinkif/static_collection/precache-manifest.abd5c252c8cdb08ce6ce86ed0c873639.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baddf39b1f24de9d7a36",
+    "url": "/css/app.977064ec.css"
+  },
+  {
+    "revision": "a483b930337922e70334",
+    "url": "/css/chunk-vendors.8587d672.css"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "2a24bf72aff3b9893261079f57f88de4",
+    "url": "/img/drunk.2a24bf72.png"
+  },
+  {
+    "revision": "59d8134e4a142158ffc1de6f3d7278f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baddf39b1f24de9d7a36",
+    "url": "/js/app.50f3e678.js"
+  },
+  {
+    "revision": "a483b930337922e70334",
+    "url": "/js/chunk-vendors.d3fc2553.js"
+  },
+  {
+    "revision": "9c3bfb9ba7c0f0cdabc1b6f5f66f8dd3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,13 @@
 
 cd /backend/drinkif/
 
+ls -lah
+
+pwd
+
 python3 manage.py migrate
+
+python3 manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
 /usr/local/bin/gunicorn -e DJANGO_SETTINGS_MODULE="$APP_NAME".settings "$APP_NAME".wsgi:application -b :8000 -w 2 --timeout 900
