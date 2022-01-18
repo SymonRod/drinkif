f8d2437d1ae7ab933b9f0e6d1737fdebcbdf4c76
SymonRod <simone.rodino@gmail.com>
2022-01-18 15:41:53 +0100

    Gunicorn launch update

--- backend/start.sh
+++ backend/start.sh
@@ -11,4 +11,4 @@ python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
-/usr/local/bin/gunicorn -e DJANGO_SETTINGS_MODULE="$APP_NAME".settings "$APP_NAME".wsgi:application -b :8000 -w 2 --timeout 900
+gunicorn drinkif.wsgi 
